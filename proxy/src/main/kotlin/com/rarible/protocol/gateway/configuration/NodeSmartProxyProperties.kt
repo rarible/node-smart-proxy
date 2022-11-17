67377449207bbc39fe66a739145220c3ebc7aaf2
sanufriev <s.anufriev@rarible.com>
2022-11-17 20:51:49 +0700

    Move src code to model

--- proxy/src/main/kotlin/com/rarible/protocol/gateway/configuration/NodeSmartProxyProperties.kt
+++ proxy/src/main/kotlin/com/rarible/protocol/gateway/configuration/NodeSmartProxyProperties.kt
@@ -2,7 +2,6 @@ package com.rarible.protocol.gateway.configuration
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
-import java.net.URI
 
 internal const val RARIBLE_PROTOCOL_NODE_SMART_PROXY_PROPERTIES = "node-smart-proxy"
 
