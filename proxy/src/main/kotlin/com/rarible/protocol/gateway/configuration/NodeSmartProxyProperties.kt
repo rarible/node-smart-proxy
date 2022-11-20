6875185294a1b6064a616ac420fa0a7012892608
sanufriev <s.anufriev@rarible.com>
2022-11-20 18:33:53 +0700

    feat: Improve config

--- proxy/src/main/kotlin/com/rarible/protocol/gateway/configuration/NodeSmartProxyProperties.kt
+++ proxy/src/main/kotlin/com/rarible/protocol/gateway/configuration/NodeSmartProxyProperties.kt
@@ -3,7 +3,7 @@ package com.rarible.protocol.gateway.configuration
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 
-internal const val RARIBLE_PROTOCOL_NODE_SMART_PROXY_PROPERTIES = "node-smart-proxy"
+internal const val RARIBLE_PROTOCOL_NODE_SMART_PROXY_PROPERTIES = "proxy"
 
 @ConstructorBinding
 @ConfigurationProperties(RARIBLE_PROTOCOL_NODE_SMART_PROXY_PROPERTIES)
