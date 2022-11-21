b6fc53129b5b274c37d69162fe71bcf351993067
sanufriev <s.anufriev@rarible.com>
2022-11-21 14:48:55 +0700

    feat: Improve logging

--- proxy/src/main/kotlin/com/rarible/protocol/gateway/service/failback/SimplePredicate.kt
+++ proxy/src/main/kotlin/com/rarible/protocol/gateway/service/failback/SimplePredicate.kt
@@ -1,14 +1,21 @@
 package com.rarible.protocol.gateway.service.failback
 
 import com.rarible.protocol.gateway.model.PossibleErrorResponse
+import org.slf4j.LoggerFactory
 
 abstract class SimplePredicate(
     private val code: Long,
     private val errorMessagePrefix: String
 ) : FailbackPredicate {
+    private val logger = LoggerFactory.getLogger(javaClass)
 
     override suspend fun needFailback(errorResponse: PossibleErrorResponse): Boolean {
         val error = errorResponse.error
-        return error?.code == code && error.message?.startsWith(errorMessagePrefix) == true
+        val responseCode = error?.code
+        val responseMessage = error?.message
+
+        return (responseCode == code && responseMessage?.startsWith(errorMessagePrefix) == true).also {
+            if (it) logger.info("Failback cause detected: code=$code, msg=${responseMessage}")
+        }
     }
 }
